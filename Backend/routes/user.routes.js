ded515f295002e390ae3b5d3898e869c2dc6b1d9
Himanshu gaur <himanshugaur055@gmail.com>
2024-10-24 09:26:46 +0530

    routes

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,15 @@ import  express from 'express';
 
 const router = express.Router()
 
-// import user routes
-
-import userRoutes from './user.routes.js';
+ 
+//signup
+router.post("/signup", async(req, res)=>{
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(400).json({message: "sign up error"})
+    }
+})
+ 
+export default router
